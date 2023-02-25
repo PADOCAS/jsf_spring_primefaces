24534b45e80cfcacb64da5310028414a0e237fb7
lucia <lucia@192.168.0.21>
2023-02-25 14:21:19 -0300

    Interface e classe abstract geral para os cadastros/ManagedBeans

---
+++ src/main/java/com/mycompany/project/cadastro/util/ActionViewPadrao.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.project.cadastro.util;
+
+import com.mycompany.project.message.util.StatusPersistencia;
+import java.io.Serializable;
+
+/**
+ *
+ * @author lucia
+ */
+public interface ActionViewPadrao extends Serializable {
+
+    public abstract void limparLista() throws Exception;
+
+    public abstract String save() throws Exception;
+
+    public abstract void saveNotReturn() throws Exception;
+
+    public abstract void saveEdit() throws Exception;
+
+    public abstract void excluir() throws Exception;
+
+    public abstract String ativar() throws Exception;
+
+    public abstract void initComponentes();
+
+    public abstract String editar() throws Exception;
+
+    public abstract void setarVariaveisNulas() throws Exception;
+
+    public abstract void consultarEntidade() throws Exception;
+
+    public abstract void statusOperation(StatusPersistencia status) throws Exception;
+
+    public abstract String redirecionarNewEntidade() throws Exception;
+
+    public abstract String redirecionarFindEntidade() throws Exception;
+
+    public abstract void addMessage(String msg) throws Exception;
+}
---
+++ src/main/java/com/mycompany/project/cadastro/util/BeanViewAbstract.java
@@ -0,0 +1,99 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.project.cadastro.util;
+
+import com.mycompany.project.message.util.Mensagem;
+import com.mycompany.project.message.util.StatusPersistencia;
+import javax.annotation.PostConstruct;
+import org.springframework.stereotype.Component;
+
+/**
+ *
+ * @author lucia
+ */
+@Component
+public abstract class BeanViewAbstract implements ActionViewPadrao {
+
+    @Override
+    public void limparLista() throws Exception {
+    }
+
+    @Override
+    public String save() throws Exception {
+        return null;
+    }
+
+    @Override
+    public void saveNotReturn() throws Exception {
+    }
+
+    @Override
+    public void saveEdit() throws Exception {
+    }
+
+    @Override
+    public void excluir() throws Exception {
+    }
+
+    @Override
+    public String ativar() throws Exception {
+        return null;
+    }
+
+    /**
+     * PostConstruct - Ao abrir a tela ja inicia os componentes, váriaveis
+     * desejadas etc..
+     */
+    @PostConstruct
+    @Override
+    public void initComponentes() {
+    }
+
+    @Override
+    public String editar() throws Exception {
+        return null;
+    }
+
+    @Override
+    public void setarVariaveisNulas() throws Exception {
+    }
+
+    @Override
+    public void consultarEntidade() throws Exception {
+    }
+
+    @Override
+    public void statusOperation(StatusPersistencia status) throws Exception {
+        if (status != null) {
+            Mensagem.responseOperation(status);
+        }
+    }
+
+    @Override
+    public String redirecionarNewEntidade() throws Exception {
+        return null;
+    }
+
+    @Override
+    public String redirecionarFindEntidade() throws Exception {
+        return null;
+    }
+
+    @Override
+    public void addMessage(String msg) throws Exception {
+        if (msg != null
+                && !msg.isEmpty()) {
+            Mensagem.msgSeverityInfo(msg);
+        }
+    }
+
+    protected void sucesso() throws Exception {
+        statusOperation(StatusPersistencia.SUCESSO);
+    }
+
+    protected void erro() throws Exception {
+        statusOperation(StatusPersistencia.ERRO);
+    }
+}
