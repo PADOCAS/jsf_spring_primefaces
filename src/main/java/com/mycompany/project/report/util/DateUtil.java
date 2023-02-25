3f77b0f605734e2bec199b99d0fee57f4642499f
lucia <lucia@192.168.0.21>
2023-02-25 11:49:32 -0300

    Classe DataUtil para conversões / jUnit

---
+++ src/main/java/com/mycompany/project/report/util/DateUtil.java
@@ -0,0 +1,58 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.project.report.util;
+
+import java.io.Serializable;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+/**
+ *
+ * @author lucia
+ */
+public class DateUtil implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * Formata data atual para colocar no nome do relatorio (ddMMyyyy)
+     * @return 
+     */
+    public static String getDateAtualReportName() {
+        DateFormat df = new SimpleDateFormat("ddMMyyyy");
+        return df.format(Calendar.getInstance().getTime());
+    }
+
+    /**
+     * Retorna data formatada para salvar no banco de dados (com as aspas) ('yyyy-MM-dd')
+     * @param data
+     * @return 
+     */
+    public static String formatDataSql(Date data) {
+        StringBuilder str = new StringBuilder();
+        DateFormat df = new SimpleDateFormat("yyyy-MM-dd");
+
+        str.append("'").append(df.format(data)).append("'");
+
+        return str.toString();
+    }
+    
+    /**
+     * Retorna data formatada para salvar no banco de dados (sem aspas) (yyyy-MM-dd)
+     * @param data
+     * @return 
+     */
+    public static String formatDataSqlSimple(Date data) {
+        StringBuilder str = new StringBuilder();
+        DateFormat df = new SimpleDateFormat("yyyy-MM-dd");
+
+        str.append(df.format(data));
+
+        return str.toString();
+    }
+
+}
---
+++ src/test/java/test/junit/TesteData.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package test.junit;
+
+import com.mycompany.project.report.util.DateUtil;
+import java.util.Calendar;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ *
+ * @author lucia
+ */
+public class TesteData {
+    
+    @Test
+    public void testData() {
+        System.out.println(DateUtil.getDateAtualReportName());
+        //Compara valores para ver se são iguais:
+        Assert.assertEquals("25022023", DateUtil.getDateAtualReportName());
+        Assert.assertEquals("'2023-02-25'", DateUtil.formatDataSql(Calendar.getInstance().getTime()));
+        Assert.assertEquals("2023-02-25", DateUtil.formatDataSqlSimple(Calendar.getInstance().getTime()));
+    }
+    
+}
