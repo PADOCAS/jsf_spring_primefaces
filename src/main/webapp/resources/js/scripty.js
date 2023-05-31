0cc1c1198a56261737095eaa64c018df197f5620
lucia <lucia@192.168.0.21>
2023-05-31 16:50:32 -0300

    Template Principal - Implementações e CSS

--- src/main/webapp/resources/js/scripty.js
+++ src/main/webapp/resources/js/scripty.js
@@ -1,3 +1,110 @@
+var arrayIdsElementsPage = new Array;
+
+/**
+ * Carrega um array global com os ids de todos os componentes da pagina Para ter
+ * faciliades em obtencao de valores dos componentes bem como trabalhar com ajax
+ */
+function carregarIdElementosPagina() {
+    arrayIdsElementsPage = new Array;
+    for (form = 0; form <= document.forms.length; form++) {
+        var formAtual = document.forms[form];
+        if (formAtual !== undefined) {
+            for (i = 0; i < document.forms[form].elements.length; i++) {
+                if (document.forms[form].elements[i].id !== '') {
+                    arrayIdsElementsPage[i] = document.forms[form].elements[i].id;
+                }
+            }
+        }
+    }
+}
+
+/**
+ * Retorno o valor do id do componente dentro do documento html passando como
+ * parametro a do id declarada em jsf
+ * 
+ * @param id
+ */
+function getValorElementPorId(id) {
+    carregarIdElementosPagina();
+    for (i = 0; i < arrayIdsElementsPage.length; i++) {
+        var valor = "" + arrayIdsElementsPage[i];
+        if (valor.indexOf(id) > -1) {
+            return valor;
+        }
+    }
+    return undefined;
+}
+
+/**
+ * Transforma nossos objetos calendar em português
+ * 
+ * @returns {undefined}
+ */
+function localeData_pt_br() {
+    PrimeFaces.locales['pt'] = {
+        closeText: 'Fechar',
+        prevText: 'Anterior',
+        nextText: 'Próximo',
+        currentText: 'Começo',
+        monthNames: ['Janeiro', 'Fevereiro', 'Marcio', 'Abril', 'Maio',
+            'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro',
+            'Dezembro'],
+        monthNamesShort: ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul',
+            'Ago', 'Set', 'Out', 'Nov', 'Dez'],
+        dayNames: ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta',
+            'Sexta', 'Sábado'],
+        dayNamesShort: ['Dom', 'Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sáb'],
+        dayNamesMin: ['D', 'S', 'T', 'Q', 'Q', 'S', 'S'],
+        weekHeader: 'Semana',
+        firstDay: 0,
+        isRTL: false,
+        showMonthAfterYear: false,
+        yearSuffix: '',
+        timeOnlyTitle: 'São Horas',
+        timeText: 'Tempo',
+        hourText: 'Hora',
+        minuteText: 'Minuto',
+        secondText: 'Segundo',
+        ampm: false,
+        month: 'Mês',
+        week: 'Semana',
+        day: 'Dia',
+        allDayText: 'Todo o Dia'
+    };
+}
+
+/**
+ * Ocutar o Menu lateral esquerdo quando necessário
+ * 
+ * @returns {undefined}
+ */
+function ocultarMenu() {
+    $('#barramenu').animate({"left": "-200px"}, "slow", function () {
+        $('#barramenu').hide();
+    });
+}
+
+/**
+ * Executar ao iniciar a página (escondendo menus etc..)
+ * 
+ * @returns {undefined}
+ */
+function initTamplate() {    
+    $(document).ready(function () {
+        $('#menupop').hide();
+        $('#barramenu').hide();
+        $('#barramenu').css("left", "-200px");
+        $('#iniciarmenu').click(function () {
+            if ($('#barramenu').is(':visible')) {
+                ocultarMenu();
+            } else {
+                $('#barramenu').show();
+                $('#barramenu').animate({"left": "0px"}, "slow");
+            }
+        });
+    });
+}
+
 //Função logout - template principal:
 function logout(contextPath) {
     //Executar essa ação com um post usando spring para invalidar sessão do usuário ao fazer logout:
@@ -15,17 +122,22 @@ function logout(contextPath) {
 
 //Função logout - seta para sair -> sair do sistema vai abrir um menu:
 function abrirMenupop() {
-    $("#menupop").show("slow").mouseleave(function() {
+    $("#menupop").show("slow").mouseleave(function () {
         fecharMenupop();
     });
 }
 
 //Função logout - seta para sair (fechar menu):
 function fecharMenupop() {
-    if($("#menupop").is(":visible")) {
+    if ($("#menupop").is(":visible")) {
         //Fechar menu com efeito slow:
         $("#menupop").hide("slow");
     }
+}
+
+//Redireciona para uma página especifica:
+function redirecionarPagina(contextPath, pagina) {
+    document.location = (contextPath + pagina + ".jsf");
 }
 
 //Invalidar Sessão - redirecionar para outra página:
