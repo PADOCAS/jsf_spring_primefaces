28aa778c64e103396dd3546d845f5b5f237fc57e
lucia <lucia@192.168.0.21>
2023-07-15 16:33:47 -0300

    Implementando Alteração de Senha

--- src/main/java/com/mycompany/project/bean/view/EntidadeBeanView.java
+++ src/main/java/com/mycompany/project/bean/view/EntidadeBeanView.java
@@ -8,9 +8,12 @@ import com.mycompany.hibernate.interfaces.crud.IInterfaceCrud;
 import com.mycompany.project.bean.geral.BeanManagedViewAbstract;
 import com.mycompany.project.bean.geral.EntidadeAlterarSenha;
 import com.mycompany.project.geral.controller.EntidadeController;
+import com.mycompany.project.message.util.Mensagem;
 import com.mycompany.project.model.Entidade;
 import com.mycompany.repository.interfaces.RepositoryLogin;
 import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.faces.bean.ManagedBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
@@ -34,6 +37,10 @@ public class EntidadeBeanView extends BeanManagedViewAbstract {
     private EntidadeController entidadeController;
 
     private EntidadeAlterarSenha entidadeAlterarSenha;
+    
+    //Injeção de dependência do DAOLogin:
+    @Autowired
+    private RepositoryLogin repositoryLogin;
 
     @Override
     public void initComponentes() {
@@ -71,21 +78,50 @@ public class EntidadeBeanView extends BeanManagedViewAbstract {
      * Método para alteração de senha do usuário
      */
     public void updateSenha() {
+        try {
+            if (entidadeAlterarSenha != null
+                    && entidadeAlterarSenha.getSenhaAtual() != null
+                    && entidadeAlterarSenha.getSenhaNova() != null
+                    && entidadeAlterarSenha.getSenhaNovaConfirmacao() != null
+                    && contextoBean != null
+                    && contextoBean.getEntidadeLogada() != null
+                    && repositoryLogin != null) {
+                //Fazer método para validar senha atual criptografando e autenticando para ver se é valida
+                validAlteracaoSenha();
+
+                //Método para valida autenticacao do usuario temos no DAOLogin! ver da onde vai ser pego na aula antes de fazer aqui.. caso precise injetar e tal..!!
+//                return repositoryLogin.autenticaUsuario(login, senha);
+            }
+        } catch (Exception ex) {
+            Logger.getLogger(EntidadeBeanView.class.getName()).log(Level.SEVERE, null, ex);
+            Mensagem.msgSeverityError("Erro ao alterar senha!<br><br>" + ex.getMessage(), "Erro");
+        }
+    }
+
+    private void validAlteracaoSenha() throws Exception {
         if (entidadeAlterarSenha != null
                 && entidadeAlterarSenha.getSenhaAtual() != null
                 && entidadeAlterarSenha.getSenhaNova() != null
                 && entidadeAlterarSenha.getSenhaNovaConfirmacao() != null
                 && contextoBean != null
-                && contextoBean.getAuthentication() != null
-                && contextoBean.getAuthentication().getName() != null) {
-            //Usuario logado: contextoBean.getAuthentication().getName()
-            
-            //Método para valida autenticacao do usuario temos no DAOLogin! ver da onde vai ser pego na aula antes de fazer aqui.. caso precise injetar e tal..!!
-//                return repositoryLogin.autenticaUsuario(login, senha);
+                && contextoBean.getEntidadeLogada() != null
+                && contextoBean.getEntidadeLogada().getEnt_login() != null
+                && repositoryLogin != null) {
+            //Senha atual válida:
+            if (!repositoryLogin.autenticaUsuario(contextoBean.getEntidadeLogada().getEnt_login(), entidadeAlterarSenha.getSenhaAtual())) {
+                throw new Exception("Senha atual não é válida!");
+            }
 
-//TODOOOOOOOOO: VERIFICAR SE SENHA NOVA E CONFIRMACAO DE SENHA NOVA BATEM            
-//TODOOOOOOOOO: VERIFICAR SE SENHA QUE O USUÁRIO DIGITOU ATUAL ESTÁ CORRETA:
-//TODOOOOOOOOO: SE TUDO OK, GRAVAR NO BANCO A NOVA SENHA CRIPTOGRAFADA!
+            //Senha atual igual a senha nova:
+            if (repositoryLogin.autenticaUsuario(contextoBean.getEntidadeLogada().getEnt_login(), entidadeAlterarSenha.getSenhaNova())) {
+                throw new Exception("Senha atual não deve ser igual a senha nova!");
+            }
+            
+            // TODOOOOOOOOOO: PAREI AQUI
+            //Senha nova e senha confirmação devem ser iguais:
+            if(!entidadeAlterarSenha.getSenhaNova().equals(entidadeAlterarSenha.getSenhaNovaConfirmacao())) {
+                throw new Exception("A nova senha e a confirmação não conferem!");
+            }
         }
     }
 
